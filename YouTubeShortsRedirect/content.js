5b822ee71f49bdbdd6292f8319b552279fcc54d7
DESKTOP-P6F255P\mdr <jggpad@gmail.com>
2024-06-08 16:17:56 +0900

    リダイレクト

--- YouTubeShortsRedirect/content.js
+++ YouTubeShortsRedirect/content.js
@@ -1,3 +1,11 @@
+// ページが読み込まれたときに現在のURLをチェックし、必要に応じてリダイレクト
+window.addEventListener('load', function() {
+    if (window.location.href.includes('youtube.com/shorts/')) {
+        var newUrl = window.location.href.replace('youtube.com/shorts/', 'youtube.com/watch?v=');
+        window.location.href = newUrl; // 新しいURLにリダイレクト
+    }
+});
+
 document.addEventListener('click', function(event) {
     var target = event.target.closest('a');
 
